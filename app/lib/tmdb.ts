53f9224d7ec5e77854582fce43490aa97b1bc21d
otm0937 <otm0937@naver.com>
2026-02-18 22:33:32 +0900

    Consolidate API requests into tmdb.ts library

--- app/lib/tmdb.ts
+++ app/lib/tmdb.ts
@@ -1,20 +1,39 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const API_KEY = process.env.TMDB_API_KEY;
+const LANG = 'ko-KR';
 
-const options = {
-  headers: {
-    accept: 'application/json',
-  },
+export const requests = {
+  fetchNowPlaying: 'movie/now_playing',
+  fetchNetflixOriginals: 'discover/tv?with_networks=213&',
+  fetchTrending: 'trending/all/week',
+  fetchTopRated: 'movie/top_rated',
+  // TODO 장르별 API는 나중에 구현
+  // fetchActionMovies: 'discover/movie?with_genres=28',
+  // fetchComedyMovies: 'discover/movie?with_genres=35',
+  // fetchHorrorMovies: 'discover/movie?with_genres=27',
+  // fetchRomanceMovies: 'discover/movie?with_genres=10749',
+  // fetchDocumentaries: 'discover/movie?with_genres=99',
+  fetchGenre: 'genre/movie/list',
 };
 
-export async function getNowPlaying() {
-  const endpoint = '/movie/now_playing';
-  const res = await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}&language=ko-KR`, {
+export async function get(endpoint: string, options: any | null = {}) {
+  const url = `${BASE_URL}/${endpoint}${
+    endpoint.includes('?') ? '&' : '?'
+  }api_key=${API_KEY}&language=${LANG}`;
+
+  const resOptions = {
+    headers: {
+      accept: 'application/json',
+    },
     ...options,
+  };
+
+  const res = await fetch(url, {
+    ...resOptions,
   });
 
   if (!res.ok) {
-    throw new Error('Failed to fetch now playing movies');
+    throw new Error(`Failed to fetch ${endpoint}`);
   }
 
   return res.json();
